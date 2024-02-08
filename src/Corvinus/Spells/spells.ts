3c221f4ce3d3da2ef7900d7de08e50a0fa0f3b44
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-02-08 10:31:42 +0100

    spells.ts created

---
+++ src/Corvinus/Spells/spells.ts
@@ -0,0 +1,22 @@
+export const spells = [
+  'xxxxxfejejeeaindaiyaiaixxxxxx', // 17
+  'jejefeai', // 3
+  'jejeai', // 0
+  'dadsafeokokok', // 0
+  'aioooofe', // 0
+  'feeai', // 2
+  'feaineain', // 1 + 2 + 2 + 2 = 7 (fe-ai-ne-ai) - not (fe-ain-ai) because 1+3+2 = 6 and 7 > 6
+  'jee', // 0
+  'fefefefefeaiaiaiaiai', // 0 (more than one 'fe')
+  'fdafafeajain', // 1 (fe-ai) 3 - 2 (because 'aj')
+  'fexxxxxxxxxxai', // 0 (3-10 = -7 < 0)
+];
+
+export const subSpells = [
+  { name: 'dai', damage: 5 },
+  { name: 'jee', damage: 3 },
+  { name: 'je', damage: 2 },
+  { name: 'ne', damage: 2 },
+  { name: 'ain', damage: 3 },
+  { name: 'ai', damage: 2 },
+];
