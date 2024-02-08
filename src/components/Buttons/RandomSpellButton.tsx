eb60f0d391dd1182a1518f6c71664c5d245ce596
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-02-08 10:23:10 +0100

    SpellButton component created

---
+++ src/components/Buttons/RandomSpellButton.tsx
@@ -0,0 +1,25 @@
+import { MouseEventHandler } from 'react';
+import MagicWandIcon from '../../assets/images/magicWand.svg?react';
+
+interface RandomSpellButtonProps {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const RandomSpellButton: React.FC<RandomSpellButtonProps> = ({
+  onClick,
+}) => {
+  return (
+    <div className='my-2 grid place-items-center'>
+      <button
+        className='rounded-md border-[1px] border-solid border-black bg-white p-1 font-semibold text-black'
+        onClick={onClick}
+      >
+        <div className='flex flex-row justify-center gap-2 p-2'>
+          <p> Cast random spell</p>
+          <MagicWandIcon className='h-6 w-6' />
+        </div>
+      </button>
+    </div>
+  );
+};
+``;
